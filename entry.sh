9f39ffaa78c958614a7011d7cd63a0d6120ecc27
Robbe Haesendonck <googleit@inuits.eu>
2022-02-16 11:41:09 +0100

    Added redis startup script Made redis start up also if the environment variables match
    Change-type: minor

--- entry.sh
+++ entry.sh
@@ -8,6 +8,17 @@ set -a
 . /usr/src/app/config/env
 set +a
 
+# Enable bash job control
+set -m
+
 echo "${TOKEN_AUTH_ROOTCERTBUNDLE}" | base64 --decode >"${CERT_FILE}"
 
-exec /usr/local/bin/docker-registry serve /etc/docker-registry.yml
+/usr/local/bin/docker-registry serve /etc/docker-registry.yml &
+
+# Start redis if env variables match
+if [ "$REGISTRY2_CACHE_ENABLED" = "true" ] && [ "$REGISTRY2_CACHE_ADDR" = "127.0.0.1:6379" ]
+then
+    /usr/bin/redis-server /etc/redis/redis.conf &
+fi
+
+fg %1
