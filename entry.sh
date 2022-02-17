a1f373d79d876465c625e36b6e0048593e358359
Robbe Haesendonck <googleit@inuits.eu>
2022-02-17 14:38:08 +0100

    Added rsyslog startup Execute rsyslogd to start up system logging
    Change-type: minor

--- entry.sh
+++ entry.sh
@@ -13,7 +13,11 @@ set -m
 
 echo "${TOKEN_AUTH_ROOTCERTBUNDLE}" | base64 --decode >"${CERT_FILE}"
 
+# Start registry
 /usr/local/bin/docker-registry serve /etc/docker-registry.yml &
+
+# Start rsyslog
+rsyslogd &
 
 # Start redis if env variables match
 if [ "$REGISTRY2_CACHE_ENABLED" = "true" ] && [ "$REGISTRY2_CACHE_ADDR" = "127.0.0.1:6379" ]
